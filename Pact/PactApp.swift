75f478a4ddabcc8b04cddb83a3871a78e110092b
yawowususnr <yawsnr@vt.edu>
2026-02-24 22:48:39 -0500

    Initial Commit

---
+++ Pact/PactApp.swift
@@ -0,0 +1,32 @@
+//
+//  PactApp.swift
+//  Pact
+//
+//  Created by Yaw Snr Owusu on 2/24/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PactApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
